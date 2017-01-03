1f1251a748c8c71a79de4453374f7f89e2fc974f
Naoki Maeda <your.infrontofdenda@yahoo.co.jp>
2017-01-03 17:27:28 +0900

    settings.pyに追記

--- zaif/settings.py
+++ zaif/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#追記
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
